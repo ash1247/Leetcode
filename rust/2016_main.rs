cc0e17effb7bce0e157e95d45b1fe2a6e23f9ff4
Ashikul Hosen <sagor.ashikul@gmail.com>
2021-12-24 00:20:08 +0600

    Completed Leetcode 2016 with two pointer solution

--- rust/2016_main.rs
+++ rust/2016_main.rs
@@ -4,7 +4,7 @@ fn main() {
     let nums = vec![1, 5, 2, 10];
     let nums1 = vec![87, 68, 91, 86, 58, 63, 43, 98, 6, 40];
     let nums2 = vec![7, 1, 5, 4];
-    let ans = maximum_difference(nums1);
+    let ans = maximum_difference_two_pointer(nums1);
     println!("{}", ans);
 }
 
@@ -20,6 +20,19 @@ pub fn maximum_difference(nums: Vec<i32>) -> i32 {
         }
         min_num = min_num.min(nums[i]);
         i += 1;
+    }
+    return max_diff;
+}
+
+pub fn maximum_difference_two_pointer(nums: Vec<i32>) -> i32 {
+    let mut lp = 0;
+    let mut max_diff = -1;
+    for rp in 1..nums.len() {
+        if nums[lp] < nums[rp] {
+            max_diff = max_diff.max(nums[rp] - nums[lp])
+        } else {
+            lp = rp
+        }
     }
     return max_diff;
 }
