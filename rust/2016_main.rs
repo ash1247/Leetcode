0ea4ecf6aadb7c2121bdd6d0014c3f6b3de37230
Ashikul Hosen <sagor.ashikul@gmail.com>
2021-12-24 00:12:12 +0600

    Completed Leetcode 2016

---
+++ rust/2016_main.rs
@@ -0,0 +1,25 @@
+use std::collections::HashMap;
+
+fn main() {
+    let nums = vec![1, 5, 2, 10];
+    let nums1 = vec![87, 68, 91, 86, 58, 63, 43, 98, 6, 40];
+    let nums2 = vec![7, 1, 5, 4];
+    let ans = maximum_difference(nums1);
+    println!("{}", ans);
+}
+
+pub fn maximum_difference(nums: Vec<i32>) -> i32 {
+    let length = nums.len();
+    let mut i = 1;
+    let mut max_diff = -1;
+    let mut min_num = nums[0];
+
+    while i < length {
+        if nums[i] > min_num {
+            max_diff = max_diff.max(nums[i] - min_num);
+        }
+        min_num = min_num.min(nums[i]);
+        i += 1;
+    }
+    return max_diff;
+}
